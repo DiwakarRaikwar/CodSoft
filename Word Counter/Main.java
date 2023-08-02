43ddbd3a4a3a54bc9eea1e1ddcc22bb0afc57e17
Diwakar <raikwarempire001@gmail.com>
2023-08-02 21:48:25 +0530

    files is been created

---
+++ Word Counter/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Here type the string or quote in which you wanna know the number of words :");
+         String in  = input.nextLine();
+
+        Arrays.sort(new int[]{3, 5, 4, 44});
+
+
+    }
+
+}
